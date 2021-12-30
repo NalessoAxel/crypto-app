a73a1e8c32e999750d560e247b22c192a0fd746a
NalessoAxel <axelnalesso@gmail.com>
2021-12-30 11:48:33 +0100

    navbar added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,24 +16,60 @@ const Navbar = () => {
     const [toggleMenu, setToggleMenu] = useState(false)
 
     return (
-        <nav className='w-full flex md:justify-center justify-between items-center px-12 pt-8'>
-            <div className='md:flex[0.5] flex-initial justify-center items-center'>
-                <img src={logo} alt="logo" className='w-32 cursor-pointer'/>
-            </div>
+			<nav className="w-full flex md:justify-center justify-between items-center p-4">
+				<div className="md:flex[0.5] flex-initial justify-center items-center">
+					<img src={logo} alt="logo" className="w-32 cursor-pointer" />
+				</div>
 
-            <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
-                {[ 'Market', 'Exchange', 'Tutorials',  'Wallet' ].map((item, index) => (
-                    <NavbarItem title={item} key={item + index} />
-                ))}
-                <li className='bg-[#2952e3] py-2 px-7 mx-2 rounded-full cursor-pointer hover:bg-[#2546bd]'>
-                    Login
-                </li>
-            </ul>
-            
-            <div className='flex relative'>
-                    {toggleMenu ? <AiOutlineClose fontSize={28} className='text-white empty-hidden cursor-pointer' onClick={() => setToggleMenu(false)} /> : <HiMenuAlt4 fontSize={28} className='text-white empty-hidden cursor-pointer' onClick={() => setToggleMenu(true)}/>}
-            </div>
-        </nav>
-    )
+				<ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+					{['Market', 'Exchange', 'Tutorials', 'Wallet'].map((item, index) => (
+						<NavbarItem title={item} key={item + index} />
+					))}
+					<li className="bg-[#2952e3] py-2 px-7 mx-2 rounded-full cursor-pointer hover:bg-[#2546bd]">
+						Login
+					</li>
+				</ul>
+
+				<div className="flex relative">
+					{!toggleMenu && (
+						<HiMenuAlt4
+							fontSize={28}
+							className="text-white md:hidden cursor-pointer"
+							onClick={() => setToggleMenu(true)}
+						/>
+					)}
+
+					{toggleMenu && (
+						<AiOutlineClose
+							fontSize={28}
+							className="text-white md:hidden cursor-pointer"
+							onClick={() => setToggleMenu(false)}
+						/>
+					)}
+
+					{toggleMenu && (
+						<ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism animate-slide-in text-white">
+							<li>
+								<AiOutlineClose
+									onClick={() => setToggleMenu(false)}
+									fontSize={28}
+									className="text-white empty-hidden cursor-pointer"
+								/>
+							</li>
+
+							{['Market', 'Exchange', 'Tutorials', 'Wallet'].map(
+								(item, index) => (
+									<NavbarItem
+										title={item}
+										key={item + index}
+										classProps="my-2 text-lg"
+									/>
+								)
+							)}
+						</ul>
+					)}
+				</div>
+			</nav>
+		);
 }
 export default Navbar
